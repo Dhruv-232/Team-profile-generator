2d0ba0bac8b17bd4ff69da9ce96765152e73b111
Dhruv-232 <dhruvsharma2004@outlook.com>
2023-03-03 22:27:26 +1100

    One more test added and passed in employee.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,12 @@
 let Employee = require("../lib/Employee");
 
 test("Can create Employee object", () => {
-    const e1 = new Employee();
-    expect(typeof(e1)).toBe("object");
-  });
-  
+  const e1 = new Employee();
+  expect(typeof (e1)).toBe("object");
+});
+
+test("getRole() should return \"Employee\"", () => {
+  const expValue = "Employee";
+  const e1 = new Employee("Sharma", 1, "test@test.com");
+  expect(e1.getRole()).toBe(expValue);
+});
